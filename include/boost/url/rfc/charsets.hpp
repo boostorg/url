ea7ead8ca0dc69ca8de43ed879ba35ac75a2d798
Vinnie Falco <vinnie.falco@gmail.com>
2021-12-19 17:43:07 -0800

    refactor rfc charsets

--- include/boost/url/rfc/charsets.hpp
+++ include/boost/url/rfc/charsets.hpp
@@ -17,76 +17,436 @@
 namespace boost {
 namespace urls {
 
-/** Character set for unreserved
+/** The unreserved character set type.
+
+    Objects of this type are invocable
+    with this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. This type satisfies
+    the <em>CharSet</em> requirements.
 
     @par BNF
     @code
     unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
     @endcode
-*/
-constexpr
-bnf::lut_chars
-unreserved_chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz"
-    "0123456789"
-    "-._~";
 
-/** Character set for gen-delims
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.3"
+        >2.3. Unreserved Characters (rfc3986)</a>
+
+    @see
+        @ref unreserved_chars
+*/
+#ifdef BOOST_URL_DOCS
+using unreserved_chars_t = see_below;
+#else
+struct unreserved_chars_t : bnf::lut_chars
+{
+    constexpr
+    unreserved_chars_t() noexcept
+        : bnf::lut_chars(
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "0123456789"
+            "-._~")
+    {
+    }
+};
+#endif
+
+/** The unreserved character set.
+
+    The object is invocable with
+    this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. The type of this
+    object satisfies the <em>CharSet</em>
+    requirements.
+
+    @par BNF
+    @code
+    unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.3"
+        >2.3. Unreserved Characters (rfc3986)</a>
+
+    @see
+        @ref unreserved_chars_t
+*/
+constexpr unreserved_chars_t unreserved_chars{};
+
+//------------------------------------------------
+
+/** The gen-delims character set type.
+
+    Objects of this type are invocable
+    with this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. This type satisfies
+    the <em>CharSet</em> requirements.
 
     @par BNF
     @code
     gen-delims    = ":" / "/" / "?" / "#" / "[" / "]" / "@"
     @endcode
-*/
-constexpr
-bnf::lut_chars
-gen_delim_chars = ":/?#[]@";
 
-/** Character set for sub-delims
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.2"
+        >2.2. Reserved Characters (rfc3986)</a>
+
+    @see
+        @ref gen_delim_chars
+*/
+#ifdef BOOST_URL_DOCS
+using gen_delim_chars_t = see_below;
+#else
+struct gen_delim_chars_t : bnf::lut_chars
+{
+    constexpr
+    gen_delim_chars_t() noexcept
+        : bnf::lut_chars(
+            ":/?#[]@")
+    {
+    }
+};
+#endif
+
+/** The gen-delims character set.
+
+    The object is invocable with
+    this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. The type of this
+    object satisfies the <em>CharSet</em>
+    requirements.
+
+    @par BNF
+    @code
+    gen-delims    = ":" / "/" / "?" / "#" / "[" / "]" / "@"
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.2"
+        >2.2. Reserved Characters (rfc3986)</a>
+
+    @see
+        @ref gen_delim_chars_t
+*/
+constexpr gen_delim_chars_t gen_delim_chars{};
+
+//------------------------------------------------
+
+/** The sub-delims character set type.
+
+    Objects of this type are invocable
+    with this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. This type satisfies
+    the <em>CharSet</em> requirements.
 
     @par BNF
     @code
     sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
                   / "*" / "+" / "," / ";" / "="
     @endcode
-*/
-constexpr
-bnf::lut_chars
-subdelim_chars = "!$&()*+,;=\x27";
 
-/** Character set for pchar
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.2"
+        >2.2. Reserved Characters (rfc3986)</a>
+
+    @see
+        @ref subdelim_chars
+*/
+#ifdef BOOST_URL_DOCS
+using subdelim_chars_t = see_below;
+#else
+struct subdelim_chars_t : bnf::lut_chars
+{
+    constexpr
+    subdelim_chars_t() noexcept
+        : bnf::lut_chars(
+            "!$&()*+,;=\x27")
+    {
+    }
+};
+#endif
+
+/** The sub-delims character set.
+
+    The object is invocable with
+    this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. The type of this
+    object satisfies the <em>CharSet</em>
+    requirements.
+
+    @par BNF
+    @code
+    sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+                  / "*" / "+" / "," / ";" / "="
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.2"
+        >2.2. Reserved Characters (rfc3986)</a>
+
+    @see
+        @ref subdelim_chars_t
+*/
+constexpr subdelim_chars_t subdelim_chars{};
+
+//------------------------------------------------
+
+/** The pchars character set type.
+
+    Objects of this type are invocable
+    with this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. This type satisfies
+    the <em>CharSet</em> requirements.
 
     @par BNF
     @code
     pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
     @endcode
-*/
-constexpr
-bnf::lut_chars
-pchars = unreserved_chars + subdelim_chars + ':' + '@';
 
-/** Character set for query
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref pchars
+*/
+#ifdef BOOST_URL_DOCS
+using pchars_t = see_below;
+#else
+struct pchars_t : bnf::lut_chars
+{
+    constexpr
+    pchars_t() noexcept
+        : bnf::lut_chars(
+            unreserved_chars +
+            subdelim_chars +
+            ':' + '@')
+    {
+    }
+};
+#endif
+
+/** The pchar character set.
+
+    The object is invocable with
+    this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. The type of this
+    object satisfies the <em>CharSet</em>
+    requirements.
+
+    @par BNF
+    @code
+    pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref pchars_t
+*/
+constexpr pchars_t pchars{};
+
+//------------------------------------------------
+
+/** The query character set type.
+
+    Objects of this type are invocable
+    with this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. This type satisfies
+    the <em>CharSet</em> requirements.
 
     @par BNF
     @code
     query         = *( pchar / "/" / "?" )
     @endcode
-*/
-constexpr
-bnf::lut_chars
-query_chars = pchars + '/' + '?';
 
-/** Character set for fragment
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.4"
+        >3.4. Query (rfc3986)</a>
+
+    @see
+        @ref query_chars_t
+*/
+#ifdef BOOST_URL_DOCS
+using query_chars_t = see_below;
+#else
+struct query_chars_t : bnf::lut_chars
+{
+    constexpr
+    query_chars_t() noexcept
+        : bnf::lut_chars(
+            pchars + '/' + '?')
+    {
+    }
+};
+#endif
+
+/** The query character set.
+
+    The object is invocable with
+    this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. The type of this
+    object satisfies the <em>CharSet</em>
+    requirements.
+
+    @par BNF
+    @code
+    query         = *( pchar / "/" / "?" )
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.4"
+        >3.4. Query (rfc3986)</a>
+
+    @see
+        @ref query_chars_t
+*/
+constexpr query_chars_t query_chars{};
+
+//------------------------------------------------
+
+/** The fragment character set type.
+
+    Objects of this type are invocable
+    with this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. This type satisfies
+    the <em>CharSet</em> requirements.
 
     @par BNF
     @code
     fragment      = *( pchar / "/" / "?" )
     @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.5"
+        >3.5. Fragment (rfc3986)</a>
+
+    @see
+        @ref fragment_chars
 */
-constexpr
-bnf::lut_chars
-fragment_chars = pchars + '/' + '?';
+#ifdef BOOST_URL_DOCS
+using fragment_chars_t = see_below;
+#else
+struct fragment_chars_t : bnf::lut_chars
+{
+    constexpr
+    fragment_chars_t() noexcept
+        : bnf::lut_chars(
+            pchars + '/' + '?')
+    {
+    }
+};
+#endif
+
+/** The fragment character set.
+
+    The object is invocable with
+    this equivalent signature:
+
+    @code
+    bool( char ch ) const noexcept;
+    @endcode
+
+    The function object returns `true` when
+    `ch` is a member of the character set,
+    and `false` otherwise. The type of this
+    object satisfies the <em>CharSet</em>
+    requirements.
+
+    @par BNF
+    @code
+    fragment      = *( pchar / "/" / "?" )
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.5"
+        >3.5. Fragment (rfc3986)</a>
+
+    @see
+        @ref fragment_chars_t
+*/
+constexpr fragment_chars_t fragment_chars{};
 
 } // urls
 } // boost
--- test/unit/rfc/charsets.cpp
+++ test/unit/rfc/charsets.cpp
@@ -9,6 +9,7 @@
 
 // Test that header file is self-contained.
 #include <boost/url/rfc/charsets.hpp>
+#include <boost/static_assert.hpp>
 
 #include "test_bnf.hpp"
 #include "test_suite.hpp"
@@ -16,23 +17,62 @@
 namespace boost {
 namespace urls {
 
-class char_sets_test
+BOOST_STATIC_ASSERT(bnf::is_charset<unreserved_chars_t>::value);
+BOOST_STATIC_ASSERT(bnf::is_charset<gen_delim_chars_t>::value);
+BOOST_STATIC_ASSERT(bnf::is_charset<subdelim_chars_t>::value);
+BOOST_STATIC_ASSERT(bnf::is_charset<pchars_t>::value);
+BOOST_STATIC_ASSERT(bnf::is_charset<query_chars_t>::value);
+BOOST_STATIC_ASSERT(bnf::is_charset<fragment_chars_t>::value);
+
+class charsets_test
 {
 public:
     void
     testLuts()
     {
         test_char_set(
+            unreserved_chars,
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                "abcdefghijklmnopqrstuvwxyz"
+                "0123456789"
+                "-._~");
+
+        test_char_set(
+            gen_delim_chars,
+                ":/?#[]@");
+
+        test_char_set(
+            subdelim_chars,
+                "!$&()*+,;=\x27");
+
+        test_char_set(
             pchars,
-            // unreserved
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             "abcdefghijklmnopqrstuvwxyz"
             "0123456789"
             "-._~"
-            // sub-delims
             "!$&'()*+,;="
-            // ":" / "@"
             ":@");
+
+        test_char_set(
+            query_chars,
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "0123456789"
+            "-._~"
+            "!$&'()*+,;="
+            ":@"
+            "/?");
+
+        test_char_set(
+            fragment_chars,
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "0123456789"
+            "-._~"
+            "!$&'()*+,;="
+            ":@"
+            "/?");
     }
 
     void
@@ -43,8 +83,8 @@ public:
 };
 
 TEST_SUITE(
-    char_sets_test,
-    "boost.url.char_sets");
+    charsets_test,
+    "boost.url.charsets");
 
 } // urls
 } // boost
