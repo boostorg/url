4ff220a241b0e83edf6f15866beb91c0e2a78341
Vinnie Falco <vinnie.falco@gmail.com>
2021-12-22 09:57:59 -0800

    path bnf docs

--- include/boost/url/rfc/paths_bnf.hpp
+++ include/boost/url/rfc/paths_bnf.hpp
@@ -34,6 +34,29 @@ struct parsed_path
 
 //------------------------------------------------
 
+/** BNF for segment
+
+    @par BNF
+    @code
+    segment       = *pchar
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_abempty_bnf,
+        @ref path_absolute_bnf,
+        @ref path_noscheme_bnf,
+        @ref path_rootless_bnf,
+        @ref pchars,
+        @ref segment_nz_bnf,
+        @ref segment_nz_nc_bnf
+*/
+#ifdef BOOST_URL_DOCS
+using segment_bnf = __see_below__;
+#else
 struct segment_bnf
 {
     pct_encoded_str& v;
@@ -47,9 +70,33 @@ struct segment_bnf
         error_code& ec,
         segment_bnf const& t);
 };
+#endif
 
 //------------------------------------------------
 
+/** BNF for segment-nz
+
+    @par BNF
+    @code
+    segment-nz    = 1*pchar
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_abempty_bnf,
+        @ref path_absolute_bnf,
+        @ref path_noscheme_bnf,
+        @ref path_rootless_bnf,
+        @ref pchars,
+        @ref segment_bnf,
+        @ref segment_nz_nc_bnf
+*/
+#ifdef BOOST_URL_DOCS
+using segment_nz_bnf = __see_below__;
+#else
 struct segment_nz_bnf
 {
     pct_encoded_str& v;
@@ -63,9 +110,36 @@ struct segment_nz_bnf
         error_code& ec,
         segment_nz_bnf const& t);
 };
+#endif
 
 //------------------------------------------------
 
+/** BNF for segment_nz_nc
+
+    @par BNF
+    @code
+    segment-nz-nc   = 1*( unreserved / pct-encoded / sub-delims / "@" )
+                    ; non-zero-length segment without any colon ":"
+    @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_abempty_bnf,
+        @ref path_absolute_bnf,
+        @ref path_noscheme_bnf,
+        @ref path_rootless_bnf,
+        @ref pchars,
+        @ref segment_bnf,
+        @ref segment_nz_bnf,
+        @ref subdelim_chars,
+        @ref unreserved_chars
+*/
+#ifdef BOOST_URL_DOCS
+using segment_nz_nc_bnf = __see_below__;
+#else
 struct segment_nz_nc_bnf
 {
     pct_encoded_str& v;
@@ -79,6 +153,7 @@ struct segment_nz_nc_bnf
         error_code& ec,
         segment_nz_nc_bnf const& t);
 };
+#endif
 
 //------------------------------------------------
 
@@ -88,7 +163,20 @@ struct segment_nz_nc_bnf
     @code
     path-abempty  = *( "/" segment )
     @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_absolute_bnf,
+        @ref path_noscheme_bnf,
+        @ref path_rootless_bnf,
+        @ref segment_bnf
 */
+#ifdef BOOST_URL_DOCS
+using path_abempty_bnf = __see_below__;
+#else
 struct path_abempty_bnf : bnf::range_
 {
     using value_type =
@@ -117,6 +205,7 @@ struct path_abempty_bnf : bnf::range_
         error_code& ec,
         pct_encoded_str& t) noexcept;
 };
+#endif
 
 //------------------------------------------------
 
@@ -126,7 +215,21 @@ struct path_abempty_bnf : bnf::range_
     @code
     path-absolute = "/" [ segment-nz *( "/" segment ) ]
     @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_abempty_bnf,
+        @ref path_noscheme_bnf,
+        @ref path_rootless_bnf,
+        @ref segment_bnf,
+        @ref segment_nz_bnf
 */
+#ifdef BOOST_URL_DOCS
+using path_absolute_bnf = __see_below__;
+#else
 struct path_absolute_bnf : bnf::range_
 {
     using value_type =
@@ -155,6 +258,7 @@ struct path_absolute_bnf : bnf::range_
         error_code& ec,
         pct_encoded_str& t) noexcept;
 };
+#endif
 
 //------------------------------------------------
 
@@ -164,7 +268,21 @@ struct path_absolute_bnf : bnf::range_
     @code
     path-noscheme = segment-nz-nc *( "/" segment )
     @endcode
+
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_abempty_bnf,
+        @ref path_absolute_bnf,
+        @ref path_rootless_bnf,
+        @ref segment_bnf,
+        @ref segment_nz_nc_bnf
 */
+#ifdef BOOST_URL_DOCS
+using path_noscheme_bnf = __see_below__;
+#else
 struct path_noscheme_bnf : bnf::range_
 {
     using value_type =
@@ -193,17 +311,38 @@ struct path_noscheme_bnf : bnf::range_
         error_code& ec,
         pct_encoded_str& t) noexcept;
 };
+#endif
 
 //------------------------------------------------
 
 /** BNF for path-rootless grammar.
 
+    @par Example
+    @code
+    using bnf::parse;
+    path_rootless_bnf t;
+    bool success = parse( it, end, ec, t);
+    @endcode
+
     @par BNF
     @code
     path-rootless = segment-nz *( "/" segment )
     @endcode
-*/
 
+    @par Specification
+    @li <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-3.3"
+        >3.3. Path (rfc3986)</a>
+
+    @see
+        @ref path_abempty_bnf,
+        @ref path_absolute_bnf,
+        @ref path_noscheme_bnf,
+        @ref segment_bnf,
+        @ref segment_nz_nc_bnf
+*/
+#ifdef BOOST_URL_DOCS
+using path_rootless_bnf = __see_below__;
+#else
 struct path_rootless_bnf : bnf::range_
 {
     using value_type =
@@ -232,6 +371,7 @@ struct path_rootless_bnf : bnf::range_
         error_code& ec,
         pct_encoded_str& t) noexcept;
 };
+#endif
 
 } // urls
 } // boost
