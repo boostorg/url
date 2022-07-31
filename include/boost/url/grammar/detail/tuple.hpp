13e59a6cec9b4b76be36bb9cc639339f6cd12c2d
Vinnie Falco <vinnie.falco@gmail.com>
2022-07-31 14:45:42 -0700

    More tuple tidying

--- include/boost/url/grammar/detail/tuple.hpp
+++ include/boost/url/grammar/detail/tuple.hpp
@@ -1,5 +1,6 @@
 //
 // Copyright (c) 2016-2019 Damian Jarek (damian dot jarek93 at gmail dot com)
+// Copyright (c) 2022 Vinnie Falco (vinnie dot falco at gmail dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,8 +13,9 @@
 
 #include <boost/url/detail/config.hpp>
 #include <boost/url/error_code.hpp>
-#include <boost/mp11/integer_sequence.hpp>
 #include <boost/mp11/algorithm.hpp>
+#include <boost/mp11/function.hpp>
+#include <boost/mp11/integer_sequence.hpp>
 #include <boost/type_traits/remove_cv.hpp>
 #include <boost/type_traits/copy_cv.hpp>
 #include <cstdlib>
@@ -29,16 +31,16 @@ struct tuple_element_impl
 {
     T t;
 
-    tuple_element_impl() = default;
-
     constexpr
-    tuple_element_impl(T const& t_)
+    tuple_element_impl(
+        T const& t_)
         : t(t_)
     {
     }
 
     constexpr
-    tuple_element_impl(T&& t_)
+    tuple_element_impl(
+        T&& t_)
         : t(std::move(t_))
     {
     }
@@ -61,11 +63,9 @@ struct tuple_impl;
 
 template<class... Ts, std::size_t... Is>
 struct tuple_impl<
-    boost::mp11::index_sequence<Is...>, Ts...>
+    mp11::index_sequence<Is...>, Ts...>
   : tuple_element_impl<Is, Ts>...
 {
-    tuple_impl() = default;
-
     template<class... Us>
     constexpr
     explicit
@@ -78,21 +78,34 @@ struct tuple_impl<
 
 template<class... Ts>
 struct tuple : tuple_impl<
-    boost::mp11::index_sequence_for<
+    mp11::index_sequence_for<
         Ts...>, Ts...>
 {
-    tuple() = default;
-
-    constexpr
-    tuple(tuple const&) noexcept = default;
-
-    template<class... Us>
+    template<class... Us,
+        class = typename std::enable_if<
+            mp11::mp_bool<
+                mp11::mp_all<std::is_constructible<
+                    Ts, Us>...>::value &&
+                ! mp11::mp_all<std::is_convertible<
+                    Us, Ts>...>::value>::value>::type
+    >
     constexpr
     explicit
     tuple(Us&&... us) noexcept
-      : tuple_impl<
-            boost::mp11::index_sequence_for<Ts...>, Ts...>{
-          std::forward<Us>(us)...}
+      : tuple_impl<mp11::index_sequence_for<
+            Ts...>, Ts...>{std::forward<Us>(us)...}
+    {
+    }
+
+    template<class... Us, int = 0,
+        class = typename std::enable_if<
+            mp11::mp_all<std::is_convertible<
+                Us, Ts>...>::value>::type
+    >
+    constexpr
+    tuple(Us&&... us) noexcept
+      : tuple_impl<mp11::index_sequence_for<
+            Ts...>, Ts...>{std::forward<Us>(us)...}
     {
     }
 };
@@ -129,27 +142,16 @@ get(tuple_element_impl<I, T&>&& te)
     return te.t;
 }
 
-template <std::size_t I, class T>
-using tuple_element = typename boost::copy_cv<
-    mp11::mp_at_c<typename remove_cv<T>::type, I>, T>::type;
+template<std::size_t I, class T>
+using tuple_element =
+    typename boost::copy_cv<
+        mp11::mp_at_c<typename
+            remove_cv<T>::type,
+            I>, T>::type;
 
 } // detail
 } // grammar
 } // urls
 } // boost
-
-#if 0
-namespace std {
-template<class T>
-struct tuple_size;
-template<class... Ts>
-struct tuple_size<
-    ::boost::urls::grammar::detail::tuple<Ts...>>
-    : std::integral_constant<
-        std::size_t, sizeof...(Ts)>
-{
-};
-} // std
-#endif
 
 #endif
