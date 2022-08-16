68c53b6192805fe67bc723eff4c1a5ccdb8b3687
alandefreitas <alandefreitas@gmail.com>
2022-08-15 21:40:10 -0300

    path tests
    fix #403

--- test/unit/doc_container.cpp
+++ test/unit/doc_container.cpp
@@ -17,7 +17,6 @@
 
 namespace boost {
 namespace urls {
-namespace grammar {
 
 struct doc_container_test
 {
@@ -32,6 +31,42 @@ struct doc_container_test
     //]
 
     void
+    path_segments()
+    {
+        auto check = [](
+            string_view path,
+            std::initializer_list<string_view> segs,
+            bool path_abs)
+        {
+            auto r1 = parse_path(path);
+            BOOST_TEST(r1.has_value());
+            auto ss = r1.value();
+            BOOST_TEST_EQ(segs.size(), ss.size());
+            BOOST_TEST_EQ(path_abs, r1->is_absolute());
+            auto it0 = segs.begin();
+            auto it1 = ss.begin();
+            while (it0 != segs.end())
+            {
+                BOOST_TEST_EQ(*it0, *it1);
+                ++it0;
+                ++it1;
+            }
+        };
+
+        check("", { }, false);
+        check("/", { }, true);
+        check("./", { "" }, false);
+        check("./usr", { "usr" }, false);
+        check("/index.htm", { "index.htm" }, true);
+        check("/images/cat-pic.gif", { "images", "cat-pic.gif" }, true);
+        check("images/cat-pic.gif", { "images", "cat-pic.gif" }, false);
+        check("/fast//query", { "fast", "", "query" }, true);
+        check("fast//", { "fast", "", "" }, false);
+        check("/./", { "" }, true);
+        check(".//", { "", "" }, false);
+    }
+
+    void
     run()
     {
         // segs()
@@ -39,6 +74,8 @@ struct doc_container_test
             url_view u;
             segs(u);
         }
+
+        path_segments();
     }
 };
 
@@ -46,6 +83,5 @@ TEST_SUITE(
     doc_container_test,
     "boost.url.doc.container");
 
-} // grammar
 } // urls
 } // boost
