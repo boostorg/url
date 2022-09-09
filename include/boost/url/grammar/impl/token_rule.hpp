1d2dc50feb0d7faa2e998d58a81a4ba1a286cefe
alandefreitas <alandefreitas@gmail.com>
2022-09-09 16:04:07 -0300

    update links

--- include/boost/url/grammar/impl/token_rule.hpp
+++ include/boost/url/grammar/impl/token_rule.hpp
@@ -4,7 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Official repository: https://github.com/CPPAlliance/http_proto
+// Official repository: https://github.com/boostorg/http_proto
 //
 
 #ifndef BOOST_URL_IMPL_GRAMMAR_TOKEN_RULE_HPP
