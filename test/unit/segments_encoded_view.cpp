64aeb34a5e9f161c7477ef85dbafb0306f0e15aa
Vinnie Falco <vinnie.falco@gmail.com>
2022-09-16 11:24:15 -0700

    segments tests

--- test/unit/segments_encoded_view.cpp
+++ test/unit/segments_encoded_view.cpp
@@ -44,79 +44,7 @@ BOOST_STATIC_ASSERT(
 struct segments_const_encoded_view_test
 {
     void
-    check(
-        string_view s,
-        std::initializer_list<
-            Type::reference> match)
-    {
-        auto rv = parse_uri_reference(s);
-        if(! BOOST_TEST(rv.has_value()))
-            return;
-        Type const& ps(rv->encoded_segments());
-        BOOST_TEST_EQ(ps.buffer().data(), s.data());
-        BOOST_TEST_EQ(ps.is_absolute(), s.starts_with('/'));
-        BOOST_TEST_EQ(ps.empty(), match.size() == 0);
-        if(! BOOST_TEST_EQ(ps.size(), match.size()))
-            return;
-        if(match.size() > 0 && ! ps.empty())
-        {
-            BOOST_TEST_EQ(ps.front(), *match.begin());
-            BOOST_TEST_EQ(ps.back(), *std::prev(match.end()));
-        }
-        // forward
-        {
-            auto it0 = ps.begin();
-            auto it1 = match.begin();
-            auto const end = ps.end();
-            while(it0 != end)
-            {
-                Type::reference r0(*it0);
-                Type::reference r1(*it1);
-                BOOST_TEST_EQ(r0, r1);
-                BOOST_TEST_EQ(*it0, *it1);
-                BOOST_TEST_EQ( // arrow
-                    it0->size(), it1->size());
-                Type::value_type v0(*it0);
-                Type::value_type v1(*it1);
-                BOOST_TEST_EQ(v0, *it1);
-                BOOST_TEST_EQ(v1, *it1);
-                auto prev = it0++;
-                BOOST_TEST_NE(prev, it0);
-                BOOST_TEST_EQ(++prev, it0);
-                ++it1;
-                BOOST_TEST_EQ(v0, v1);;
-            }
-        }
-        // reverse
-        if(match.size() > 0)
-        {
-            auto const begin = ps.begin();
-            auto it0 = ps.end();
-            auto it1 = match.end();
-            do
-            {
-                auto prev = it0--;
-                BOOST_TEST_NE(prev, it0);
-                BOOST_TEST_EQ(--prev, it0);
-                --it1;
-                Type::reference r0(*it0);
-                Type::reference r1(*it1);
-                BOOST_TEST_EQ(*it0, *it1);
-                BOOST_TEST_EQ(r0, r1);
-            }
-            while(it0 != begin);
-        }
-        // ostream
-        {
-            std::stringstream ss;
-            ss << ps;
-            BOOST_TEST_EQ(ss.str(),
-                rv->encoded_path());
-        }
-    }
-
-    void
-    testMembers()
+    testSpecialMembers()
     {
         // segments_encoded_view()
         {
@@ -186,31 +114,6 @@ struct segments_const_encoded_view_test
     }
 
     void
-    testRange()
-    {
-    /*  Legend
-
-        '#' %23     '?' %3F
-        '.' %2E     '[' %5B
-        '/' %2F     ']' %5D
-    */
-        check( "", {});
-        check( "./", { "" });
-        check( ".//", { "", "" });
-        check( "/", {});
-        check( "/./", { "" });
-        check( "/.//", { "", "" });
-        check( "/%3F", {"%3F"});
-        check( "%2E/", {"%2E", ""});
-        check( "./usr", { "usr" });
-        check( "/index.htm", { "index.htm" });
-        check( "/images/cat-pic.gif", { "images", "cat-pic.gif" });
-        check( "images/cat-pic.gif", { "images", "cat-pic.gif" });
-        check( "/fast//query", { "fast", "", "query" });
-        check( "fast//",  { "fast", "", "" });
-    }
-
-    void
     testJavadocs()
     {
         // {class}
@@ -235,8 +138,7 @@ struct segments_const_encoded_view_test
     void
     run()
     {
-        testMembers();
-        testRange();
+        testSpecialMembers();
         testJavadocs();
     }
 };
