0a37bddaf5ecc37c437b7b7286340acfd38c850a
alandefreitas <alandefreitas@gmail.com>
2022-10-10 14:22:48 -0300

    update copyright notices

--- test/unit/snippets.cpp
+++ test/unit/snippets.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2022 Alan Freitas (alandefreitas@gmail.com)
+// Copyright (c) 2022 Alan de Freitas (alandefreitas@gmail.com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
