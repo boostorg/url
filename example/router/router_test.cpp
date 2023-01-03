f77c816a964c7230b79e9947b41bdccd23200290
alandefreitas <alandefreitas@gmail.com>
2023-01-03 17:13:07 -0300

    [fold] copyright

--- example/router/router_test.cpp
+++ example/router/router_test.cpp
@@ -1,5 +1,4 @@
 //
-// Copyright (c) 2019 Vinnie Falco (vinnie.falco@gmail.com)
 // Copyright (c) 2022 Alan de Freitas (alandefreitas@gmail.com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
