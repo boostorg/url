eabbb868248fe3447ce11556389fcc8c5412f92b
alandefreitas <alandefreitas@gmail.com>
2023-04-13 15:52:10 -0300

    [FOLD] error_cat no constinit

--- src/error.cpp
+++ src/error.cpp
@@ -83,11 +83,7 @@ case error::missing_pct_hexdig:
 # pragma warning( disable : 4592 )
 #endif
 
-#if defined(__cpp_constinit) && __cpp_constinit >= 201907L
-constinit error_cat_type error_cat;
-#else
 error_cat_type error_cat;
-#endif
 
 #if defined(_MSC_VER) && _MSC_VER <= 1900
 # pragma warning( pop )
--- src/grammar/error.cpp
+++ src/grammar/error.cpp
@@ -106,13 +106,8 @@ message(
 # pragma warning( disable : 4592 )
 #endif
 
-#if defined(__cpp_constinit) && __cpp_constinit >= 201907L
-constinit error_cat_type error_cat;
-constinit condition_cat_type condition_cat;
-#else
 error_cat_type error_cat;
 condition_cat_type condition_cat;
-#endif
 
 #if defined(_MSC_VER) && _MSC_VER <= 1900
 # pragma warning( pop )
