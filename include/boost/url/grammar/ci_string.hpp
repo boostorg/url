9c9850f1882d8a9c14f09b4b3ee3f517534d2b35
Alan de Freitas <alandefreitas@gmail.com>
2026-02-27 18:33:22 -0500

    fix: ci_equal arguments by const reference

--- include/boost/url/grammar/ci_string.hpp
+++ include/boost/url/grammar/ci_string.hpp
@@ -295,8 +295,8 @@ struct ci_equal
         class String0, class String1>
     bool
     operator()(
-        String0 s0,
-        String1 s1) const noexcept
+        String0 const& s0,
+        String1 const& s1) const noexcept
     {
         return ci_is_equal(s0, s1);
     }
